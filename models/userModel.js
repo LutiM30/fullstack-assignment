9c6e5f80df076d18c70708bede10c2491cd8d7e1
Mitul Mistry <mitmistry311@gmail.com>
2024-03-14 16:50:55 -0400

    upda : license number will be encrypted

--- models/userModel.js
+++ models/userModel.js
@@ -3,6 +3,7 @@ const { default: mongoose } = require("mongoose");
 const userDocument = require("./documents/userDocument");
 const carDocument = require("./documents/carDocument");
 const internalFields = require("./documents/internalFields");
+const consts = require("../utils/consts");
 
 const Schema = mongoose.Schema;
 const Model = mongoose.model;
@@ -11,6 +12,19 @@ const getNewSchema = (document) => new Schema(document, internalFields);
 
 const carSchema = getNewSchema(carDocument);
 const userSchema = getNewSchema(userDocument(carSchema));
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  bcrypt.hash(
+    user.license_number,
+    consts.howManyTimesEncrypt,
+    (error, hash) => {
+      user.license_number = hash;
+      console.log({ user });
+      next();
+    }
+  );
+});
 
 // export mongoose model
 const userModel = Model("users", userSchema);
--- utils/consts/index.js
+++ utils/consts/index.js
@@ -2,6 +2,7 @@ const consts = {
   mongo: {
     upsert: { upsert: true },
   },
+  howManyTimesEncrypt: 5,
 };
 
 module.exports = consts;
